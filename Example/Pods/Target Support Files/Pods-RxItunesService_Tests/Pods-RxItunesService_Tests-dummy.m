1ec7266e90cd9b3dd41569a1ad56d8d5c350382e
Alexandre Freire <xandrefreire@gmail.com>
2018-06-01 09:21:59 +0100

    Create pod

---
+++ Example/Pods/Target Support Files/Pods-RxItunesService_Tests/Pods-RxItunesService_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxItunesService_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RxItunesService_Tests
+@end
---
+++ Example/Pods/Target Support Files/RxItunesService/RxItunesService-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RxItunesService : NSObject
+@end
+@implementation PodsDummy_RxItunesService
+@end
